4ff27780ee9592db3000f82f985de9bbebdbd9a5
jimin0127 <ckdgh4662@naver.com>
2020-11-13 03:35:40 +0900

    버튼 클릭 C# script

---
+++ BetweenNoodle/Assets/papperButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Runtime.Serialization;
+
+
+public class papperButton : MonoBehaviour
+{
+    public Sprite newsprite;
+    
+    private Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(fClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void fClick() {
+        Debug.Log("Button Click");
+        button.image.sprite = newsprite;
+
+    }
+}
---
+++ BetweenNoodle/Assets/settingSwitch.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+ 
+
+public class settingSwitch : MonoBehaviour
+{
+    public Sprite newsprite; //selectedOOn
+    private Button EffectButton; //효과음
+    private Button SoundButton; //배경음 
+    private Button Button;
+    
+    private Sprite Onsprite; //selectedOff
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SoundButton = GameObject.Find("SoundButton").GetComponent<Button>();
+        EffectButton = GameObject.Find("EffectButton").GetComponent<Button>();
+        Button = GameObject.Find("Button").GetComponent<Button>();
+
+        Onsprite = SoundButton.image.sprite;
+
+        SoundButton.onClick.AddListener( () => switchChange(SoundButton));
+        EffectButton.onClick.AddListener( () => switchChange(EffectButton));
+        Button.onClick.AddListener( () => switchChange(Button));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void switchChange(Button button) {
+        if(button.image.sprite == newsprite){
+            button.image.sprite = Onsprite;
+        }
+        else {
+            button.image.sprite = newsprite;
+        }
+    }
+}
