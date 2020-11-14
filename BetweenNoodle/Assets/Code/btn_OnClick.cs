c83f47def81e378e617cf989329447ca901e25a7
Sonjiwoo <sonjiu00@gamil.com>
2020-11-14 17:48:31 +0900

    첫화면 완성

---
+++ BetweenNoodle/Assets/Code/btn_OnClick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class btn_OnClick : MonoBehaviour
+{
+    public void GoRule()
+    {
+        SceneManager.LoadScene("Rule");
+    }
+    public void GoSet()
+    {
+        SceneManager.LoadScene("SettingScene");
+    }
+    public void GoCheck()
+    {
+        SceneManager.LoadScene("AttendanceScene");
+    }
+    public void GoStage()
+    {
+        SceneManager.LoadScene("Stage");
+    }
+    public void GoShop()
+    {
+        SceneManager.LoadScene("Clothes");
+    }
+}
